887ddaa3429188528f8a54d14a04c9c0f579d770
hshan_ <85825489+hshan-1@users.noreply.github.com>
2022-05-12 21:21:23 +0200

    lazyy

--- UserCreator/ui.h
+++ UserCreator/ui.h
@@ -46,6 +46,7 @@ namespace UserCreator {
 	private: System::Windows::Forms::Button^ newUser;
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Timer^ blocade;
+	private: System::Windows::Forms::Label^ loginfo;
 	private: System::ComponentModel::IContainer^ components;
 
 	private:
@@ -62,88 +63,66 @@ namespace UserCreator {
 		void InitializeComponent(void)
 		{
 			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(ui::typeid));
 			this->login = (gcnew System::Windows::Forms::TextBox());
 			this->passwd = (gcnew System::Windows::Forms::TextBox());
 			this->getaccs = (gcnew System::Windows::Forms::Button());
 			this->newUser = (gcnew System::Windows::Forms::Button());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->blocade = (gcnew System::Windows::Forms::Timer(this->components));
+			this->loginfo = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// login
 			// 
 			this->login->BackColor = System::Drawing::SystemColors::ActiveCaption;
 			this->login->BorderStyle = System::Windows::Forms::BorderStyle::None;
-			this->login->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20));
-			this->login->Location = System::Drawing::Point(12, 163);
-			this->login->MaxLength = 32;
+			resources->ApplyResources(this->login, L"login");
 			this->login->Name = L"login";
-			this->login->Size = System::Drawing::Size(484, 31);
-			this->login->TabIndex = 0;
-			this->login->Text = L"Login";
-			this->login->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
 			this->login->Click += gcnew System::EventHandler(this, &ui::login_TextChanged);
+			this->login->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &ui::login_KeyDown);
 			// 
 			// passwd
 			// 
 			this->passwd->BackColor = System::Drawing::SystemColors::ActiveCaption;
 			this->passwd->BorderStyle = System::Windows::Forms::BorderStyle::None;
-			this->passwd->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20));
-			this->passwd->Location = System::Drawing::Point(12, 252);
-			this->passwd->MaxLength = 16;
+			resources->ApplyResources(this->passwd, L"passwd");
 			this->passwd->Name = L"passwd";
-			this->passwd->PasswordChar = '#';
-			this->passwd->Size = System::Drawing::Size(484, 31);
-			this->passwd->TabIndex = 1;
-			this->passwd->Text = L"Login";
-			this->passwd->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
-			this->passwd->WordWrap = false;
 			this->passwd->Click += gcnew System::EventHandler(this, &ui::passwd_TextChanged);
 			this->passwd->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &ui::passwd_KeyDown);
 			// 
 			// getaccs
 			// 
+			this->getaccs->BackColor = System::Drawing::Color::Transparent;
 			this->getaccs->FlatAppearance->BorderSize = 0;
-			this->getaccs->FlatAppearance->MouseDownBackColor = System::Drawing::Color::Black;
-			this->getaccs->FlatAppearance->MouseOverBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(224)),
-				static_cast<System::Int32>(static_cast<System::Byte>(224)), static_cast<System::Int32>(static_cast<System::Byte>(224)));
-			this->getaccs->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->getaccs->Location = System::Drawing::Point(143, 303);
+			this->getaccs->FlatAppearance->MouseDownBackColor = System::Drawing::Color::Transparent;
+			this->getaccs->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Transparent;
+			resources->ApplyResources(this->getaccs, L"getaccs");
 			this->getaccs->Name = L"getaccs";
-			this->getaccs->Size = System::Drawing::Size(75, 66);
-			this->getaccs->TabIndex = 2;
-			this->getaccs->Text = L"Log in";
-			this->getaccs->UseVisualStyleBackColor = true;
+			this->getaccs->UseVisualStyleBackColor = false;
 			this->getaccs->Click += gcnew System::EventHandler(this, &ui::getaccs_Click);
 			// 
 			// newUser
 			// 
+			this->newUser->BackColor = System::Drawing::Color::Transparent;
 			this->newUser->FlatAppearance->BorderSize = 0;
-			this->newUser->FlatAppearance->MouseDownBackColor = System::Drawing::Color::Black;
-			this->newUser->FlatAppearance->MouseOverBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(224)),
-				static_cast<System::Int32>(static_cast<System::Byte>(224)), static_cast<System::Int32>(static_cast<System::Byte>(224)));
-			this->newUser->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->newUser->Location = System::Drawing::Point(288, 303);
+			this->newUser->FlatAppearance->MouseDownBackColor = System::Drawing::Color::Transparent;
+			this->newUser->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Transparent;
+			resources->ApplyResources(this->newUser, L"newUser");
 			this->newUser->Name = L"newUser";
-			this->newUser->Size = System::Drawing::Size(75, 66);
-			this->newUser->TabIndex = 3;
-			this->newUser->Text = L"Create\nnew user";
-			this->newUser->UseVisualStyleBackColor = true;
+			this->newUser->UseVisualStyleBackColor = false;
 			this->newUser->Click += gcnew System::EventHandler(this, &ui::newUser_Click);
 			// 
 			// button1
 			// 
+			this->button1->BackColor = System::Drawing::Color::Transparent;
+			this->button1->DialogResult = System::Windows::Forms::DialogResult::Cancel;
 			this->button1->FlatAppearance->BorderSize = 0;
 			this->button1->FlatAppearance->MouseDownBackColor = System::Drawing::Color::Transparent;
-			this->button1->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Red;
-			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->button1->Location = System::Drawing::Point(216, 368);
+			this->button1->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Transparent;
+			resources->ApplyResources(this->button1, L"button1");
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 66);
-			this->button1->TabIndex = 4;
-			this->button1->Text = L"\tquit\n";
-			this->button1->TextAlign = System::Drawing::ContentAlignment::TopCenter;
-			this->button1->UseVisualStyleBackColor = true;
+			this->button1->UseVisualStyleBackColor = false;
 			this->button1->Click += gcnew System::EventHandler(this, &ui::button1_Click);
 			// 
 			// blocade
@@ -151,25 +130,33 @@ namespace UserCreator {
 			this->blocade->Interval = 1000;
 			this->blocade->Tick += gcnew System::EventHandler(this, &ui::blocade_Tick);
 			// 
+			// loginfo
+			// 
+			resources->ApplyResources(this->loginfo, L"loginfo");
+			this->loginfo->ForeColor = System::Drawing::Color::DarkRed;
+			this->loginfo->Name = L"loginfo";
+			// 
 			// ui
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AcceptButton = this->getaccs;
+			resources->ApplyResources(this, L"$this");
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
-			this->ClientSize = System::Drawing::Size(508, 485);
-			this->ControlBox = false;
+			this->CancelButton = this->button1;
+			this->Controls->Add(this->loginfo);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->newUser);
 			this->Controls->Add(this->getaccs);
 			this->Controls->Add(this->passwd);
 			this->Controls->Add(this->login);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->MaximizeBox = false;
+			this->MinimizeBox = false;
 			this->Name = L"ui";
 			this->ShowIcon = false;
 			this->ShowInTaskbar = false;
-			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
-			this->Text = L"ui";
+			this->SizeGripStyle = System::Windows::Forms::SizeGripStyle::Show;
 			this->TopMost = true;
-			this->TransparencyKey = System::Drawing::Color::Black;
 			this->Load += gcnew System::EventHandler(this, &ui::ui_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
@@ -180,17 +167,18 @@ namespace UserCreator {
 		int a = 0;
 
 	private: System::Void ui_Load(System::Object^ sender, System::EventArgs^ e) {
-		MessageBox::Show("Welcome to user creator\nlogger\npasswordcheccker\nmooltitool\naudioplayer\ndemonaslayer");
+		//MessageBox::Show("Welcome to user creator\nlogger\npasswordcheccker\nmooltitool\naudioplayer\ndemonaslayer");
 	}
 	private: void control(String^ login, String^ passwd)//checks log info 
 	{
 		if (login=="admin" && passwd == "admin1")
 		{
 			Beep(1000, 100);
-			MessageBox::Show("WELCOME", "HELLO", MessageBoxButtons::YesNoCancel);
-		}
+			MessageBox::Show("WELCOME", "HELLO", MessageBoxButtons::OK);
+			}
 		else
 		{
+			this->loginfo->Visible = true;
 			guard();//control sends info to guard 
 		}
 	}
@@ -242,17 +230,28 @@ private: System::Void passwd_TextChanged(System::Object^ sender, System::EventAr
 	this->passwd->Text = "";
 }
 public: System::Void getaccs_Click(System::Object^ sender, System::EventArgs^ e){
-	this->button1->Location = System::Drawing::Point(216, 368);
+	this->loginfo->Visible = false;
+	this->button1->Location = System::Drawing::Point(208, 356);
 	String^ login = this->login->Text;
 	String^ passwd = this->passwd->Text;
 	int sLength = this->passwd->TextLength;
 	control(login, passwd);
 	Beep(1000, 100);
 }
+private: System::Void login_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e) {
+	if ("")
+	{
+		Random^ rand = gcnew Random;
+		int tone = rand->Next(3, 9);
+		Beep(tone * 100, 100);
+	}
+}
 private: System::Void passwd_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e) {
 	if ("")
 	{
-		Beep(220, 100);
+		Random^ rand = gcnew Random;
+		int tone =rand->Next(3,9);
+		Beep(tone*100, 100);
 	}
 }
 private: System::Void blocade_Tick(System::Object^ sender, System::EventArgs^ e) {
@@ -268,6 +267,5 @@ private: System::Void newUser_Click(System::Object^ sender, System::EventArgs^ e
 	
 	
 }
-
 };
 }
